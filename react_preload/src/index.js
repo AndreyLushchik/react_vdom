8a3ddd8ace630a2194e485e7d8d367e905713467
AndreyLushchik <redbull199556@gmail.com>
2021-12-06 18:48:31 +0300

    create basic files

---
+++ react_preload/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import SayHi from "./components/conditional/index";
+
+/*ReactDOM.render(
+	<React.StrictMode>
+		<SayHi />
+	</React.StrictMode>,
+	document.getElementById("root")
+);
+*/
