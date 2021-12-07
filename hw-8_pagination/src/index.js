d9eed2273b8056aa5e38f0e06578c33fc4cdb5ce
AndreyLushchik <redbull199556@gmail.com>
2021-12-07 22:09:18 +0300

    create pagination basic

---
+++ hw-8_pagination/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Pagintaion from "./components/Pagination";
+
+ReactDOM.render(
+	<React.StrictMode>
+		<Pagintaion />
+	</React.StrictMode>,
+	document.getElementById("root")
+);
