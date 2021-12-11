e737b7d0735e39ea09048eb3f1fcb712f5aca9c8
AndreyLushchik <redbull199556@gmail.com>
2021-12-11 19:40:19 +0300

    add last change

--- hw-8_pagination/src/components/Pagination/index.js
+++ hw-8_pagination/src/components/Pagination/index.js
@@ -2,13 +2,13 @@ import React, { useState, useEffect } from "react";
 let btnArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 function Pagination() {
-	const [picture, setPicture] = useState([]);
+	const [pictures, setPictures] = useState([]);
 	const getPicture = async (elem) => {
 		const response = await fetch(
 			`https://jsonplaceholder.typicode.com/photos?_page=${elem}&_limit=${10}`
 		);
 		const dataPicture = await response.json();
-		setPicture(dataPicture);
+		setPictures(dataPicture);
 	};
 	useEffect(() => {
 		getPicture();
@@ -17,7 +17,7 @@ function Pagination() {
 	return (
 		<div className="content">
 			<div className="picture">
-				{picture.map((post) => {
+				{pictures.map((post) => {
 					return (
 						<div key={post.id}>
 							<img
