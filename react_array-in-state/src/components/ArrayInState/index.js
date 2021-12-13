beac752363d266919bbc10b28d926cd310656910
AndreyLushchik <redbull199556@gmail.com>
2021-12-13 17:24:55 +0300

    add task train array

--- react_array-in-state/src/components/ArrayInState/index.js
+++ react_array-in-state/src/components/ArrayInState/index.js
@@ -1,3 +1,19 @@
 import React, { useState } from "react";
-function ArrState(params) {}
+import names from "../../names.json";
+function ArrState() {
+	const [people, setPeople] = useState(["Andrey", "Grigory", "Anna"]);
+	const newName = () => {
+		const newMember = names[Math.floor(Math.random() * 13)];
+		const prevArr = [...people, newMember];
+		setPeople(prevArr);
+	};
+	return (
+		<div>
+			<button onClick={newName}>Add name</button>
+			{people.map((name) => {
+				return <span key={Math.floor(Math.random() * 1_000_000)}>{name}</span>;
+			})}
+		</div>
+	);
+}
 export default ArrState;
