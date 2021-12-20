85d78042759bb7f18431a7dcb6a2d7a2846b04b6
AndreyLushchik <redbull199556@gmail.com>
2021-12-20 13:52:00 +0300

    create basic files

---
+++ router/src/Navigator/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Circle from "../Components/Circle";
+import Square from "../Components/Square";
+
+export default function Navigator() {
+	return <Router></Router>;
+}
