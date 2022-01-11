f3b4626b2862abf380cc8e16103a42da6d41b926
AndreyLushchik <redbull199556@gmail.com>
2022-01-11 23:38:06 +0300

    add redirect from work page

--- router/src/navigation/index.js
+++ router/src/navigation/index.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import Figure from "../Components/Figure";
 
 export default function Navigator() {
 	return (
 		<Router>
+			<Redirect from="/" to="/circle/red" />
 			<Route path="/:figure/:colored">
 				<Figure />
 			</Route>
